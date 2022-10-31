093d3fd14593d624edb09f04bc74cfe695a3bfcb
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2022-10-31 17:06:59 +0200

    Added Mapper Static Asign Logic

--- tema1.cpp
+++ tema1.cpp
@@ -4,10 +4,17 @@ using namespace std;
 
 #define NUMBER_OF_ARGUMENTS 4
 
-typedef struct argument argument;
+typedef struct map_argument argument;
+typedef struct file_numbers file_numbers;
 
-struct argument {
+struct file_numbers {
+    string file_name;
+    vector<int> numbers;
+};
 
+struct map_argument {
+    vector<file_numbers> files;
+    int exponent;
 };
 
 
@@ -21,11 +28,32 @@ int string_to_number(string value) {
 
 void *create_exponent_lists(void *arg) {
 
-
-
+    map_argument *data = (map_argument *) arg;
+    
+    
 
 
     return NULL;
+}
+
+void prioritize_files_on_threads(map_argument mapper_arguments[], vector<int> numbers[], string files_names[], int mapper_threads_number, int nr_files, int exponent) {
+    multiset<pair<int, int>> priority;
+    for (int i = 0; i < mapper_threads_number; ++i) {
+        priority.insert({0, i});
+        mapper_arguments[i].exponent = exponent;
+    }
+
+    for (int i = 0; i < nr_files; ++i) {
+        auto first = *priority.begin();
+        int new_workload = first.first + numbers[i].size();
+        int id = first.second;
+        file_numbers file;
+        file.file_name = files_names[i];
+        file.numbers = numbers[i];
+        mapper_arguments[id].files.push_back(file);
+        priority.erase(priority.begin());
+        priority.insert({new_workload, id});
+    }
 }
 
 
@@ -39,14 +67,14 @@ int main(int argc, char *argv[]) {
     ifstream in(argv[3]);
     int nr_files;
     in >> nr_files;
-    string files[nr_files];
+    string files_names[nr_files];
     for (int i = 0; i < nr_files; ++i) {
-        in >> files[i];
+        in >> files_names[i];
     }
 
     vector<int> numbers[nr_files];
     for (int i = 0; i < nr_files; ++i) {
-        ifstream read(files[i]);
+        ifstream read(files_names[i]);
         int nr_numbers;
         read >> nr_numbers;
         for (int j = 1; j <= nr_numbers; ++j) {
@@ -55,20 +83,22 @@ int main(int argc, char *argv[]) {
             numbers[i].push_back(number);
         }
     }
-    
+
     int mapper_threads_number = string_to_number(argv[1]);
     pthread_t mapper_threads[mapper_threads_number];
-    argument mapper_arguments[mapper_threads_number];
-    
+    map_argument mapper_arguments[mapper_threads_number];
+
     int reducer_threads_number = string_to_number(argv[2]);
     pthread_t reducer_threads[reducer_threads_number];
-    argument reducer_arguments[reducer_threads_number];
+    // argument reducer_arguments[reducer_threads_number];
+
+    prioritize_files_on_threads(mapper_arguments, numbers, files_names, mapper_threads_number, nr_files, reducer_threads_number);
+
+    
+    
 
     int error = 0;
     for (int i = 0; i < mapper_threads_number; ++i) {
-
-        // mapper_arguments[i].
-
         error = pthread_create(&mapper_threads[i], NULL, create_exponent_lists, &mapper_arguments[i]);
         if (error) {
             cout << "Error: Creation of thread with number " << i << " failed!";
